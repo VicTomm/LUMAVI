4f60469fa5c085045e6cb922b2e1fcab337a7f51
LuisFer Anaya <luisfer.anaya20@gmail.com>
2017-02-08 21:24:54 -0600

    Logo and web section animations added using GSAP and MagicScroll, images folder added containing lumavi's logo parts, index.html, app.css & app.js modified

--- js/app.js
+++ js/app.js
@@ -1,1 +1,42 @@
 $(document).foundation()
+
+// init controller
+	var controller = new ScrollMagic.Controller();
+
+var tl = new TimelineMax();
+var tlLogo = new TimelineMax();
+
+var mockup = $(".mockup");
+var mockups = $(".mockups");
+var blue = $("#blue");
+var red = $("#red");
+var orange = $("#orange");
+var points = $("#points");
+var lumavi = $("#lumavi-text");
+
+tlLogo
+  .fromTo(blue, 1, {opacity:0, y:'-=100'}, {opacity:1, y:'0'}, 1)
+  .fromTo(red, 1, {opacity:0, y:'+=100'}, {opacity:1, y:'0'}, '-=1')
+  .fromTo(orange, 1, {opacity:0, x:'+=100'}, {opacity:1, x:'0'}, '-=1')
+  .fromTo(points, 1, {opacity:0}, {opacity:1})
+  .fromTo(lumavi, 1, {opacity:0, x:'+=100'}, {opacity:1, x:'0'});
+
+var tween = tl
+  .fromTo(mockup, 1, {opacity:0}, {opacity:1})
+  .fromTo(mockups[0], 0.5, {opacity:0, x:'-=200'}, {opacity:1, x:'0'})
+  .fromTo(mockups[1], 0.5, {opacity:0, x:'+=200'}, {opacity:1, x:'0'})
+  .fromTo(mockups[2], 0.5, {opacity:0}, {opacity:1})
+  .fromTo(mockups[3], 0.5, {opacity:0}, {opacity:1}, '-=0.3')
+  .fromTo(mockups[4], 0.5, {opacity:0}, {opacity:1}, '-=0.3')
+  .fromTo(mockups[5], 0.5, {opacity:0}, {opacity:1}, '-=0.3')
+  .fromTo(mockups[6], 0.5, {opacity:0, x:'-=200'}, {opacity:1, x:'0'}, '-=0.5')
+  .fromTo(mockups[7], 0.5, {opacity:0, x:'+=200'}, {opacity:1, x:'0'}, '-=0.5')
+  .fromTo(mockups[8], 0.5, {opacity:0, y:'+=100'}, {opacity:1, y:'0'})
+  .fromTo(mockups[9], 0.5, {opacity:0, y:'+=100'}, {opacity:1, y:'0'});
+
+// build scene
+var scene = new ScrollMagic.Scene({
+	triggerElement: "#services"
+})
+.setTween(tween)
+.addTo(controller);
