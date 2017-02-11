df53789612184ee0442f9ccdcd28d330926986e8
Victor Manuel Tommasi Uribe <vtomm6575@gmail.com>
2017-02-11 00:18:00 -0600

    Menus laterales, se tienen que cambiar al lado derecho welp!

--- js/app.js
+++ js/app.js
@@ -40,3 +40,22 @@ var scene = new ScrollMagic.Scene({
 })
 .setTween(tween)
 .addTo(controller);
+
+$(function() {
+  
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    $removeClass = $("ul li a").removeClass('selected');
+    $(this).addClass('selected');
+    
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
